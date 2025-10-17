b5480cbe68b2dd6ad3ed164ac9fbe336d3a1002c
markmatakili2 <markmatakili84@gmail.com>
2025-10-17 17:27:13 +0300

    updates

--- contact_files/src_app_contact_page_tsx_7db61a0f._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_08bd8fb4._.js",
-  "static/chunks/src_components_2e52f63c._.js"
-],
-    source: "dynamic"
-});
--- realestate_files/src_app_real-estate_page_tsx_7db61a0f._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_146a9c66._.js",
-  "static/chunks/src_components_ui_8eb58db1._.js"
-],
-    source: "dynamic"
-});
